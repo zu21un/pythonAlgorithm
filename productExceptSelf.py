caa02868d0ae84a17b399cb3b0e089e63e244968
zu21un <68384717+zu21un@users.noreply.github.com>
2021-05-31 23:34:22 +0900

    21.05.31

---
+++ productExceptSelf.py
@@ -0,0 +1,16 @@
+def productExceptSelf(self, nums:List[int])->List[int]:
+    out = []
+    p = 1
+    #왼쪽 곱셈
+    for i in range(0, len(nums)):
+        out.append(p)
+        p = p * nums[i]
+    
+    p = 1
+
+    #오른쪽 곱셈
+    for i in range(len(nums) - 1, 0 - 1, -1):
+        out[i] = out[i] * p
+        p = p * nums[i]
+    
+    return out
